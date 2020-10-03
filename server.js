7519e673559acc2ba5515271896e4c8747963566
Dmitry Sokun <dmitry.sokun@element.global>
2020-10-04 02:09:44 +0300

    Index.htlm

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
-app.use(express.static('./'));
+app.use(express.static('./static/'));
 
 app.listen(PORT, function () {
     console.log(`Example app listening on port ${PORT}!`);
