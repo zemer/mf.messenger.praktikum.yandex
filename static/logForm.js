8bfaba4e61c2c552f5e751bac69f0556fff1ca57
Dmitry Sokun <dmitry.sokun@element.global>
2020-10-04 01:39:30 +0300

    Сбор данных из формы

---
+++ static/logForm.js
@@ -0,0 +1,14 @@
+function logForm() {
+    let form = document.forms[0];
+
+    let log = {};
+
+    for (const e of form.elements) {
+        if (e['type'] === 'text' || e['type'] === 'password')
+            log[e['id']] = e['value'];
+    }
+
+    console.log(log);
+
+    return false;
+}
