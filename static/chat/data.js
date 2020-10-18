f031aa6d9d5f74cce8cfce8d422836f0a83f2ddd
Dmitry Sokun <dmitry.sokun@element.global>
2020-10-18 16:08:52 +0300

    Шаблон Chat

---
+++ static/chat/data.js
@@ -0,0 +1,19 @@
+import ChatItem from "../components/chat-item/chat-item.js";
+export const items = [
+    new ChatItem({
+        user: "Илья",
+        preview: "Текст",
+        date: "01.10.2020"
+    }),
+    new ChatItem({
+        user: "Илларион",
+        preview: "Изображение",
+        date: "02.10.2020"
+    }),
+    new ChatItem({
+        user: "Игорь",
+        preview: "Текст",
+        date: "03.10.2020"
+    }),
+];
+//# sourceMappingURL=data.js.map
