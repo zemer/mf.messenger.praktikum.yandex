d9ec33fe3a6fb1a3d5dabbabd1d2788424def60e
Dmitry Sokun <dmitry.sokun@element.global>
2020-11-08 00:22:23 +0300

    Типы Handlebars

--- src/global.d.ts
+++ src/global.d.ts
@@ -5,3 +5,7 @@ declare global {
         new <TSource extends object, TTarget extends object>(target: TSource, handler: ProxyHandler<TSource>): TTarget;
     }
 }
+
+import * as HandlebarsModule from 'handlebars';
+
+declare const Handlebars: typeof HandlebarsModule;
