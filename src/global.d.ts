dd5e94ede8a9d5fa65387fd045aa1ef1392761f3
Dmitry Sokun <dmitry.sokun@element.global>
2020-11-09 08:25:54 +0300

    Рефакторинг

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,11 +1,9 @@
-export { };
+import * as HandlebarsModule from 'handlebars';
 
 declare global {
     interface ProxyConstructor {
         new <TSource extends object, TTarget extends object>(target: TSource, handler: ProxyHandler<TSource>): TTarget;
     }
 }
-
-import * as HandlebarsModule from 'handlebars';
 
 declare const Handlebars: typeof HandlebarsModule;
