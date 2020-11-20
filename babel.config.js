38ee91f2f2ac2708b4f34349b93081a746a4b8ec
Dmitry Sokun <dmitry.sokun@element.global>
2020-11-20 09:32:14 +0300

    Удален babel

--- babel.config.js
+++
@@ -1,5 +0,0 @@
-//В проекте использую tsc. Однако для сборки тестов нужен babel.
-//Я следовал этой инструкции: https://jestjs.io/docs/en/getting-started#using-typescript
-module.exports = {
-    presets: [["@babel/preset-env", { targets: { node: "current" } }]],
-};
